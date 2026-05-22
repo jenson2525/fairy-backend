6948229e57e4a96a7e94401a7e4355baea40428c
JensonXu-NUAA <1515356530@qq.com>
2026-05-22 23:34:39 +0800

    refactor: 清理残留的 chat 模块功能代码

--- fairy-common/src/main/java/cn/nuaa/jensonxu/fairy/common/repository/mysql/mapper/CustomChatMessageMapper.java
+++
@@ -1,8 +0,0 @@
-package cn.nuaa.jensonxu.fairy.common.repository.mysql.mapper;
-
-import cn.nuaa.jensonxu.fairy.common.repository.mysql.data.CustomChatMessageDO;
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-
-public interface CustomChatMessageMapper extends BaseMapper<CustomChatMessageDO> {
-
-}
